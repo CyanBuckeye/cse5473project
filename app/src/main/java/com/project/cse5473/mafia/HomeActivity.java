355a18aaa97a89b181db006ff373a29d810c2d9d
Evan Clark <clark.2095@osu.edu>
2017-04-03 02:06:02 -0400

    Initial commit. Simple app with input box and button to 'send' message

---
+++ app/src/main/java/com/project/cse5473/mafia/HomeActivity.java
@@ -0,0 +1,72 @@
+package com.project.cse5473.mafia;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class HomeActivity extends AppCompatActivity {
+
+    EditText messageInputBox;
+    TextView receivedMessagesText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        messageInputBox = (EditText) findViewById(R.id.msg_input);
+        receivedMessagesText = (TextView) findViewById(R.id.received_msgs);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    // Send the message the user entered
+    public void sendMessage(View v){
+        // for now, just append to received messages
+        String input = messageInputBox.getText().toString();
+        messageInputBox.setText("");
+
+        receivedMessagesText.append("\n"+input);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
