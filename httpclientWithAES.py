d2a3886eba7d4a249a73670fccac026b4bd5a016
CyanBuckeye <xu.2727@osu.edu>
2017-04-25 19:34:45 -0400

    Server and Client (in python) with AES Encryption
    Block Size: 16 Mode: CBC

---
+++ httpclientWithAES.py
@@ -0,0 +1,83 @@
+import json
+import httplib
+import pdb
+from Crypto.Cipher import AES
+from Crypto import Random
+import base64
+import hashlib
+
+class AESCipher:
+    def __init__(self, raw_key):
+        self.bs = 16
+        self.raw_key = raw_key
+        self.key = hashlib.sha256(raw_key.encode()).digest()
+    
+        
+    def _pad(self, s):#PKCS5 Padding
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+    
+    def _unpad(self, s):#remove padding
+        return s[:-ord(s[-1])]
+                 
+    def encrypt(self, plaintxt):
+        msg = self._pad(plaintxt)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(msg))
+        
+    def decrypt(self, ciphertxt):
+        ciphertxt = base64.b64decode(ciphertxt)
+        iv = ciphertxt[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return self._unpad(cipher.decrypt(ciphertxt[AES.block_size:])).decode('utf-8')
+
+aesWrapper = AESCipher('my private key')
+
+def join(name):
+    conn = httplib.HTTPConnection('', 8000)
+    data = {"type": 1, "msg":name}
+    data = json.dumps(data)
+    #conn.request("GET", "/")
+    headers = {"Content-type": "application/json"}
+    ciphertxt = aesWrapper.encrypt(data)
+    conn.request("POST", "", ciphertxt, headers)
+    r1 = conn.getresponse()
+
+    data = r1.read()
+    data = aesWrapper.decrypt(data)    
+    pk = json.loads(data.decode())
+    print(pk['state'], str(pk['msg']))
+    conn.close()
+
+def query(name):
+    conn = httplib.HTTPConnection('', 8000)
+    data = {"type": 0, "msg":name}
+    data = json.dumps(data)
+    headers = {"Content-type": "application/json"}
+    ciphertxt = aesWrapper.encrypt(data)
+    conn.request("POST", "", ciphertxt, headers)
+    r1 = conn.getresponse()
+    print(r1.status, r1.reason)
+
+    data = r1.read()
+    data = aesWrapper.decrypt(data)  
+    pk = json.loads(data.decode())
+    print(pk['state'], str(pk['msg']))
+    conn.close()
+
+def vote(name):
+    conn = httplib.HTTPConnection('', 8000)
+    data = {"type": 2, "msg":name}
+    data = json.dumps(data)
+    headers = {"Content-type": "application/json"}
+    ciphertxt = aesWrapper.encrypt(data)
+    conn.request("POST", "", ciphertxt, headers)
+    r1 = conn.getresponse()
+    print(r1.status, r1.reason)
+
+    data = r1.read()
+    data = aesWrapper.decrypt(data)  
+    pk = json.loads(data.decode())
+    print(pk['state'], str(pk['msg']))
+    conn.close()
+
